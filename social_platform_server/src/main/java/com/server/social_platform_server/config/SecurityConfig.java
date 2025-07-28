3d9dacc006c05450fffdc4c753c593a0da4f3c15
Goian-Sergiu <sergiu_goian@yahoo.com>
2025-07-28 23:02:09 +0300

    security config

---
+++ social_platform_server/src/main/java/com/server/social_platform_server/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.server.social_platform_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable()) // disable csrf for apis
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // allow loign, register, verify
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // use default security login
+        return http.build();
+    }
+}
