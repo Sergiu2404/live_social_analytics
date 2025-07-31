23e6fbcd610c795dbe166501ca865719ac122385
Goian-Sergiu <sergiu_goian@yahoo.com>
2025-07-31 18:33:53 +0300

    post entity + external packages

---
+++ social_platform_server/src/main/java/com/server/social_platform_server/models/post/Post.java
@@ -0,0 +1,90 @@
+package com.server.social_platform_server.models.post;
+
+import com.server.social_platform_server.models.user.User;
+import jakarta.persistence.*;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) //user is loaded only when the post is accessed (not when post is fetched)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    private String content;
+    private String mediaUrl;
+    private String classificationLabel;
+    private double latitude;
+    private double longitude;
+    private ZonedDateTime createdAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMediaUrl() {
+        return mediaUrl;
+    }
+
+    public void setMediaUrl(String mediaUrl) {
+        this.mediaUrl = mediaUrl;
+    }
+
+    public String getClassificationLabel() {
+        return classificationLabel;
+    }
+
+    public void setClassificationLabel(String classificationLabel) {
+        this.classificationLabel = classificationLabel;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
